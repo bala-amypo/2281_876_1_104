b00613e421a4726fd3420ad82d62c4bc040bcfc0
bala-amypo <bala@amypo.in>
2025-12-18 04:23:03 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceCatalogItem.java
+++ src/main/java/com/example/demo/model/DeviceCatalogItem.java
@@ -14,5 +14,79 @@ public class DeviceCatalogItem{
     private Boolean active;
     private LocalDateTime createdAt;
 
-    
+     public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(String employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getJobRole() {
+        return jobRole;
+    }
+
+    public void setJobRole(String jobRole) {
+        this.jobRole = jobRole;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public DeviceCatalogItem(String employeeId, String fullName, String email,String department, String jobRole,Boolean active, LocalDateTime createdAt) {
+        this.employeeId = employeeId;
+        this.fullName = fullName;
+        this.email = email;
+        this.department = department;
+        this.jobRole = jobRole;
+        this.active = active;
+        this.createdAt = createdAt;
+    }
+    public DeviceCatalogItem() {
+    }
 }
