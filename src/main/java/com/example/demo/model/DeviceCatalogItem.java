f31db16e1ffb52d97d11cd889f45d562abb87939
bala-amypo <bala@amypo.in>
2025-12-18 04:05:39 +0000

    User commit

--- src/main/java/com/example/demo/model/DeviceCatalogItem.java
+++ src/main/java/com/example/demo/model/DeviceCatalogItem.java
@@ -3,4 +3,16 @@ package com.example.demo.model;
 import jakarta.persistence.*;
 
 @Model
-public class 
+public class DeviceCatalogItem{
+    @Id
+    private Long id;
+    private String employeeId;
+    private String fullName;
+    private String email;
+    private String department;
+    private String jobRole;
+    private Boolean active;
+    private LocalDateTime createdAt;
+
+    
+}
